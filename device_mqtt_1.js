f890f19c008ff0bca25daf6500520a2037636906
GreenNature <nobu1015@naver.com>
2016-09-23 17:07:10 +0900

    오류 수정

--- device_mqtt_1.js
+++ device_mqtt_1.js
@@ -375,7 +375,7 @@ function MQTTClient(){
 	  var ri = "<ri>"+config.nodeID+'_'+randomInt(100000, 999999)+"</ri>";
 	  var dKey = "<dKey>"+config.dKey+"</dKey>";
 	  var cty = "<cty>application/vnd.onem2m-prsp+xml</cty>";
-    var sensor_val = config.content().split(',');
+    var sensor_val = config.contents().split(',');
 	  var reqBody = "<pc><cin><cnf>text</cnf><con>"+sensor_val[0]+tcp_device.sensor_data()+sensor_val[2]+"</con></cin></pc></m2m:req>";
 	  var createContentInstance = reqHeader+op+to+fr+ty+ri+cty+dKey+reqBody;
 	  client.publish("/oneM2M/req/"+ config.nodeID +"/"+config.AppEUI, createContentInstance, {qos : 1}, function(){
